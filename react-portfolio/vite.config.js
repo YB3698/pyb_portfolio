00e8e0b9a09c6b48230f22e4f67263cefd37c161
python_pyb <dbqls3698@naver.com>
2025-07-03 17:27:58 +0900

    카톡 미리보기 수정

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/pyb_portfolio/', // GitHub Pages 주소에 맞게
-  plugins: [react()],
-  build: {
+
+  plugins: [react()] // dist 폴더로 빌드할려면 이것만 남겨야됨.
+  
+  , build: {
     outDir: '../docs', // 루트에 docs 폴더 생성
   },
 })
